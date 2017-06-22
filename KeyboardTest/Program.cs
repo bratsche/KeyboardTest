2d51d6a8181cebe1eaf22da400f164f0bc319d4b
Cody Russell <cody@jhu.edu>
2017-06-22 14:44:16 -0500

    Keyboard test

---
+++ KeyboardTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+namespace KeyboardTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.KeyPressEvent += (o, e) => {
+                Console.WriteLine($"Key {e.Event.KeyValue}, HW {e.Event.HardwareKeycode}");
+            };
+            win.Show();
+            Application.Run();
+        }
+    }
+}
